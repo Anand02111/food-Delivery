47bf4bdb7188cf7ccb3206b9d7077a9d2979d366
Anand Tageja <atageja@gmail.com>
2021-05-26 00:31:37 +0530

    upadte

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb+srv://anand02:<password>@cluster0.vfqs0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  mongoURI: "mongodb+srv://anand02:<ranjit211>@cluster0.ditj4.mongodb.net/mernstack?retryWrites=true&w=majority",
   secretOrKey: "anand02",
   userRoles: ["user", "manager"]
 };
